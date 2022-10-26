61f3d50e471882f553fcce4feda8426c81812387
ZJ <zhijietan97@gmail.com>
2022-10-26 23:05:03 +0800

    chore: specific publicPath for asset module folder

--- webpack_basics/02_asset_modules/webpack.config.js
+++ webpack_basics/02_asset_modules/webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "./dist"),
+        publicPath: "dist/", // default is "auto"
     },
     mode: "none",
     module: {
